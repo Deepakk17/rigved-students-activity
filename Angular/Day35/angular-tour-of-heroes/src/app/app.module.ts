0af04a090aa570b834654c9b03650396918d0c40
deepak <deepakk1726@gmail.com>
2022-04-11 23:16:07 +0530

    Done

--- Angular/Day35/angular-tour-of-heroes/src/app/app.module.ts
+++ Angular/Day35/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MdfDemoComponent } from './mdf-demo/mdf-demo.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ActivityComponent } from './activity/activity.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    MdfDemoComponent,
-    ActivityComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
